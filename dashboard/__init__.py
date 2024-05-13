d75022a8a5348c381109c58c9a2c2794c1e7633f
joshua-larsen <64284030+joshua-larsen@users.noreply.github.com>
2024-05-13 12:50:50 -0600

    create pages subdirectorying and home.html file

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,14 +1,9 @@
-# app.py
-
 from flask import Flask
-from markupsafe import escape
 
-app = Flask(__name__)
+from dashboard import pages
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def create_app():
+    app = Flask(__name__)
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.register_blueprint(pages.bp)
+    return app
