e421c5e87d6f1fc091d1a8190eaf0e0dc9aa8295
joshua-larsen <64284030+joshua-larsen@users.noreply.github.com>
2024-05-15 15:10:01 -0600

    Update UI by adding navigation and new css styles

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -2,13 +2,15 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 
-from dashboard import pages, posts
+from dashboard import pages, posts, database
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_prefixed_env()
+
+    database.init_app(app)
 
     app.register_blueprint(pages.bp)
     app.register_blueprint(posts.bp)
